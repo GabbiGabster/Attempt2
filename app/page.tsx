39702502cba53e9e5589e7f7890d979d2fb9ad28
GabbiGabster <143375008+GabbiGabster@users.noreply.github.com>
2025-03-01 14:56:43 -0500

    testPage

--- app/page.tsx
+++ app/page.tsx
@@ -2,100 +2,21 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <div className="w-[100%] h-[1024px] relative bg-white  overflow-hidden">
+        <div className="w-[100%] h-[196px] left-0 top-0 absolute bg-[#a9b6d6]" />
+        <div className="flex items-center justify-center h-screen">
+          <div className="w-[149px] h-[78px] left-[328px] top-[250px] absolute text-[#294ca5] text-[50px] font-normal font-['Inter']">Login</div>
+          <div className="w-[766px] h-[65px] left-[328px] top-[415px] absolute bg-[#d9d9d9] rounded-[10px]" />
+          <div className="w-[766px] h-[65px] left-[328px] top-[577px] absolute bg-[#d9d9d9] rounded-[10px]" />
+          <div className="w-[258px] h-[65px] left-[582px] top-[709px] absolute bg-[#294ca5] rounded-[10px]" />
+          <div className="w-[598px] h-[39px] left-[328px] top-[352px] absolute text-[#7085b8] text-3xl font-normal font-['Inter']">username or email</div>
+          <div className="w-[178px] h-[39px] left-[308px] top-[509px] absolute text-center text-[#7085b8] text-3xl font-normal font-['Inter']">password</div>
+          <div className="w-[186px] h-[18px] left-[946px] top-[519px] absolute text-[#294ca5] text-lg font-normal font-['Inter']">forgot password?</div>
+          <div className="w-[311px] h-[18px] left-[821px] top-[362px] absolute"><span class="text-[#808aa4] text-lg font-normal font-['Inter']">New to Alera? </span><span class="text-[#294ca5] text-lg font-normal font-['Inter']">Create an account</span></div>
+          <div className="w-[180px] h-[33px] left-[621px] top-[725px] absolute text-center text-white text-3xl font-normal font-['Inter']">Sign In</div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
